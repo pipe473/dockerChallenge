dd48bb1b0c6c5a99176761a01c416a90cbc71757
pipe473 <bedoyafelipe743@gmail.com>
2020-11-18 12:16:51 +0100

    creating models to build albums and registers for artists

--- kenjoChallenge/src/app/models/album.ts
+++ kenjoChallenge/src/app/models/album.ts
@@ -1,2 +1,21 @@
 export class Album {
+    public album_id: number
+    public title: string
+    public artistId: string
+    public coverUrl: string
+    public year: Date
+    public genre: string
+    public createdAt: Date
+    public updatedAt: Date
+
+    constructor(album_id?:number, title?:string, artistId?:string, coverUrl?:string, year?:Date, genre?:string, createdAt?:Date, updatedAt?:Date ){
+        this.album_id = album_id;
+        this.title = title;
+        this.artistId = artistId;
+        this.coverUrl = coverUrl;
+        this.year = year;
+        this.genre = genre;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
--- kenjoChallenge/src/app/models/artist.ts
+++ kenjoChallenge/src/app/models/artist.ts
@@ -1,2 +1,17 @@
 export class Artist {
+    public artist_id: number
+    public name: string
+    public photoUrl: string
+    public birthdate: Date
+    public createdAt: Date
+    public updatedAt: Date
+
+    constructor(artist_id?:number, name?:string, photoUrl?:string, birthdate?:Date, createdAt?:Date, updatedAt?:Date){
+        this.artist_id = artist_id;
+        this.name = name;
+        this.photoUrl = photoUrl;
+        this.birthdate = birthdate;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
