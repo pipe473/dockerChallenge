17037f97a39a900c51f850fd9e810f832f896bdf
pipe473 <bedoyafelipe743@gmail.com>
2020-11-18 12:53:28 +0100

    doing calls from api to consume in the service

--- kenjoChallenge/src/app/shared/api.service.ts
+++ kenjoChallenge/src/app/shared/api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Artist } from '../models/artist';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private url = "http://localhost:3000"
+
+  constructor(private http: HttpClient) { }
+
+  postArtist(newArtist: Artist){
+    return this.http.post(this.url+"/artist", newArtist)
+  }
+
+  getArtists(){
+    return this.http.get(this.url+"/artists/all")
+  }
 }
